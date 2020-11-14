426177d43f73393a71057cca913c8424d5f5f5b7
Aclard <matgomiero@gmail.com>
2020-11-14 15:50:11 +0100

    First Gravity version

---
+++ Assets/Scripts/Management/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager
+{
+    private static GameManager gameManager = null;
+    public ManagementReferencing _ref;
+
+    public static GameManager GetSingleton()
+    {
+        if(gameManager == null)
+        {
+            gameManager = new GameManager();
+
+        }
+        return gameManager;
+    }
+}
